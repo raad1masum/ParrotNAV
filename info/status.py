62796aeaffa266dbc628812643e9607daf10ce66
raad1masum <piraadwan@gmail.com>
2020-05-26 18:35:05 -0700

    create status message

--- info/status.py
+++ info/status.py
@@ -13,3 +13,7 @@ def status():
     pitch_rate = get_info(info.pitch_rate)
     yaw_error = get_info(info.yaw_error)
     yaw_rate = get_info(info.yaw_rate)
+
+    status_message = f"Target Range: {target_range} \n X Range: {x_range} \n Y Range: {y_range} \n Z Range: {z_range} \n Roll Error: {roll_error} \n Roll Rate: {roll_rate} \n Pitch Error: {pitch_error} \n Pitch Rate: {pitch_rate} \n Yaw Error: {yaw_error} \n Yaw Rate: {yaw_rate} \n"
+
+    return status_message
