fe27b5caafb93245e46ad9122fba55e1a05f06c4
raad1masum <piraadwan@gmail.com>
2020-05-26 18:33:36 -0700

    create location for driver

--- simulation/sim.py
+++ simulation/sim.py
@@ -1,15 +1,18 @@
 from selenium import webdriver
 from time import sleep
 
-driver = webdriver.Edge(
-    '/mnt/c/Users/raad1/Documents/Webdrivers/edgedriver_win64/msedgedriver.exe')
+DRIVER_LOCATION = '/mnt/c/Users/raad1/Documents/Webdrivers/edgedriver_win64/msedgedriver.exe'
+driver = webdriver.Edge(DRIVER_LOCATION)
 driver.get('https://iss-sim.spacex.com/')
+
 
 def get_info(info):
     return driver.find_element_by_xpath(info).text
 
+
 def control(control):
     driver.find_element_by_xpath(control).click()
+
 
 sleep(30)
 
