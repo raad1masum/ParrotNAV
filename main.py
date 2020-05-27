4c5b8a183c1f74d5a74ac3af32839d886595989b
raad1masum <piraadwan@gmail.com>
2020-05-26 18:14:35 -0700

    print out info

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 from simulation import sim
 from simulation.sim import *
 from controls import controls
+from info import info
 
 control(controls.pitch_up)
+
+while True:
+    print(get_info(info.pitch_error))
