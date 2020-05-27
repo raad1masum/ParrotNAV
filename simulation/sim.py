685155e1f572e9b51136c2d6376a2b5b0dfab5bd
raad1masum <piraadwan@gmail.com>
2020-05-26 17:37:04 -0700

    remove controls from simulation

--- simulation/sim.py
+++ simulation/sim.py
@@ -5,20 +5,6 @@ driver = webdriver.Edge(
     '/mnt/c/Users/raad1/Documents/Webdrivers/edgedriver_win64/msedgedriver.exe')
 driver.get('https://iss-sim.spacex.com/')
 
-translate_left = '//*[@id="translate-left-button"]'
-translate_right = '//*[@id="translate-right-button"]'
-translate_up = '//*[@id="translate-up-button"]'
-translate_down = '//*[@id="translate-down-button"]'
-translate_forward = '//*[@id="translate-forward-button"]'
-translate_backward = '//*[@id="translate-backward-button"]'
-yaw_left = '//*[@id="yaw-left-button"]'
-yaw_right = '//*[@id="yaw-right-button"]'
-pitch_up = '//*[@id="pitch-up-button"]'
-pitch_down = '//*[@id="pitch-down-button"]'
-roll_left = '//*[@id="roll-left-button"]'
-roll_right = '//*[@id="roll-right-button"]'
-
-
 def control(control):
     driver.find_element_by_xpath(control).click()
 
