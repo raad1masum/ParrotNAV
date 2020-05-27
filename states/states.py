6cb1252326c45dd60755d3861cad69734de4fab6
raad1masum <piraadwan@gmail.com>
2020-05-26 22:55:23 -0700

    add states.py

---
+++ states/states.py
@@ -0,0 +1,13 @@
+from states.info import *
+from simulation.sim import get_info
+
+target_range = get_info(target_range_state)
+x_range = get_info(x_range_state)
+y_range = get_info(y_range_state)
+z_range = get_info(z_range_state)
+roll_error = get_info(roll_error_state)
+roll_rate = get_info(roll_rate_state)
+pitch_error = get_info(pitch_error_state)
+pitch_rate = get_info(pitch_rate_state)
+yaw_error = get_info(yaw_error_state)
+yaw_rate = get_info(yaw_rate_state)
