2ee721728a00fefd92e35e634e8c664200b31e8b
raad1masum <piraadwan@gmail.com>
2020-05-30 16:37:18 -0700

    tune bang-bang controller

--- main.py
+++ main.py
@@ -6,16 +6,22 @@ from controls import controls
 from states.states import *
 from states.status import status
 
-# control(controls.pitch_up)
+def move_left():
+    for i in range(3): 
+        control(controls.yaw_left)
+    for i in range(3): 
+        control(controls.yaw_right)
+
+
+def move_right():
+    for i in range(3): 
+        control(controls.yaw_right)
+    for i in range(3): 
+        control(controls.yaw_left)
 
 while True:
-    if float(get_info(yaw_error_state).rstrip('°')) < 0.0 and abs(float(get_info(yaw_rate_state).rstrip('°/s'))) < 0.5:
-        control(controls.yaw_left)
-        # print(float(get_info(yaw_error_state).rstrip('°')))
-        print(abs(float(get_info(yaw_rate_state).rstrip('°/s'))))
-
-    if float(get_info(yaw_error_state).rstrip('°')) > 0.0 and abs(float(get_info(yaw_rate_state).rstrip('°/s'))) <= 0.5:
-        control(controls.yaw_right)
-        # print(float(get_info(yaw_error_state).rstrip('°')))
-        print(abs(float(get_info(yaw_rate_state).rstrip('°/s'))))
-    
+    while float(get_info(yaw_error_state).rstrip('°')) != 0.0:
+        if float(get_info(yaw_error_state).rstrip('°')) < 0.0:
+            move_left()
+        if float(get_info(yaw_error_state).rstrip('°')) > 0.0:
+            move_right()
