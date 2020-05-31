0b7c05694dbb3a50660af3667cb2982b1e42f636
raad1masum <piraadwan@gmail.com>
2020-05-30 20:35:19 -0700

    implement PID

--- main.py
+++ main.py
@@ -6,22 +6,28 @@ from controls import controls
 from states.states import *
 from states.status import status
 
-def move_left():
-    for i in range(3): 
+
+kp = 1.5
+
+def move_left(correction_rate):
+    print(correction_rate)
+    for i in range(correction_rate):
         control(controls.yaw_left)
-    for i in range(3): 
+    for i in range(correction_rate):
         control(controls.yaw_right)
 
 
-def move_right():
-    for i in range(3): 
+def move_right(correction_rate):
+    print(correction_rate)
+    for i in range(correction_rate):
         control(controls.yaw_right)
-    for i in range(3): 
+    for i in range(correction_rate):
         control(controls.yaw_left)
+
 
 while True:
     while float(get_info(yaw_error_state).rstrip('°')) != 0.0:
         if float(get_info(yaw_error_state).rstrip('°')) < 0.0:
-            move_left()
+            move_left(int(abs(kp * float(get_info(yaw_error_state).rstrip('°')))))
         if float(get_info(yaw_error_state).rstrip('°')) > 0.0:
-            move_right()
+            move_right(int(abs(kp * float(get_info(yaw_error_state).rstrip('°')))))
