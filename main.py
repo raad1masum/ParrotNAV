52754b89ff029f54a855caa2ecd323e083c09094
raad1masum <piraadwan@gmail.com>
2020-05-31 20:34:07 -0700

    tune PID controller and experiment with speed

--- main.py
+++ main.py
@@ -1,31 +1,41 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
 from simulation import sim
 from simulation.sim import *
 from controls import controls
 from states.states import *
 from states.status import status
 
-
 kp = 1.5
 
+list = []
+
+
 def move_left(correction_rate):
+    list.append(correction_rate)
     print(correction_rate)
     for i in range(correction_rate):
         control(controls.yaw_left)
     for i in range(correction_rate):
         control(controls.yaw_right)
+    print(list)
 
 
 def move_right(correction_rate):
+    list.append(correction_rate)
     print(correction_rate)
     for i in range(correction_rate):
         control(controls.yaw_right)
     for i in range(correction_rate):
         control(controls.yaw_left)
+    print(list)
 
 
-while True:
-    while float(get_info(yaw_error_state).rstrip('°')) != 0.0:
-        if float(get_info(yaw_error_state).rstrip('°')) < 0.0:
-            move_left(int(abs(kp * float(get_info(yaw_error_state).rstrip('°')))))
-        if float(get_info(yaw_error_state).rstrip('°')) > 0.0:
-            move_right(int(abs(kp * float(get_info(yaw_error_state).rstrip('°')))))
+# while True:
+while float(get_info(yaw_error_state).rstrip('°')) != 0.0:
+    if float(get_info(yaw_error_state).rstrip('°')) < 0.0:
+        move_left(int(abs(kp * float(get_info(yaw_error_state).rstrip('°')))))
+    if float(get_info(yaw_error_state).rstrip('°')) > 0.0:
+        move_right(
+            int(abs(kp * float(get_info(yaw_error_state).rstrip('°')))))
