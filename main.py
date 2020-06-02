6335f510643a8f27a196970ab0a63ff5d664027e
raad1masum <piraadwan@gmail.com>
2020-06-02 10:36:43 -0700

    fix less than 1 error correction

--- main.py
+++ main.py
@@ -15,24 +15,33 @@ list = []
 def move_left(correction_rate):
     list.append(correction_rate)
     print(correction_rate)
-    for i in range(correction_rate):
+    if correction_rate == 0:
         control(controls.yaw_left)
-    for i in range(correction_rate):
         control(controls.yaw_right)
+    else:
+        for i in range(correction_rate):
+            control(controls.yaw_left)
+        for i in range(correction_rate):
+            control(controls.yaw_right)
     print(list)
 
 
 def move_right(correction_rate):
     list.append(correction_rate)
     print(correction_rate)
-    for i in range(correction_rate):
+    if correction_rate == 0:
         control(controls.yaw_right)
-    for i in range(correction_rate):
         control(controls.yaw_left)
+    else:
+        for i in range(correction_rate):
+            control(controls.yaw_right)
+        for i in range(correction_rate):
+            control(controls.yaw_left)
     print(list)
 
 
-while float(get_info(yaw_error_state).rstrip('°')) != 0.0:
+while True:
+#while float(get_info(yaw_error_state).rstrip('°')) != 0.0:
     if float(get_info(yaw_error_state).rstrip('°')) < 0.0:
         move_left(int(abs(kp * float(get_info(yaw_error_state).rstrip('°')))))
     if float(get_info(yaw_error_state).rstrip('°')) > 0.0:
