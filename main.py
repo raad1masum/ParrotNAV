2be63665c70ba20af072c2d95cbc2404ffa1b18d
raad1masum <piraadwan@gmail.com>
2020-06-02 18:17:25 -0700

    remove old imports

--- main.py
+++ main.py
@@ -2,10 +2,10 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 from simulation import sim
-from simulation.sim import *
-from controls import controls
-from states.states import *
-from states.status import status
+# from simulation.sim import *
+# from controls import controls
+# from states.states import *
+# from states.status import status
 
 from controller import correct_yaw
 
