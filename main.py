e81f640b3a8f9f913246576a825f40da735101ee
raad1masum <piraadwan@gmail.com>
2020-06-02 17:14:52 -0700

    automatically plot data

--- main.py
+++ main.py
@@ -7,36 +7,38 @@ from controls import controls
 from states.states import *
 from states.status import status
 
-kp = 1.5
+kp = 1
 
 list = []
 
 
 def move_left(correction_rate):
-    list.append(correction_rate)
-    print(correction_rate)
     if correction_rate == 0:
         control(controls.yaw_left)
         control(controls.yaw_right)
+        list.append(float(get_info(yaw_error_state).rstrip('°')))
     else:
         for i in range(correction_rate):
             control(controls.yaw_left)
+            list.append(float(get_info(yaw_error_state).rstrip('°')))
         for i in range(correction_rate):
             control(controls.yaw_right)
+            list.append(float(get_info(yaw_error_state).rstrip('°')))
     print(list)
 
 
 def move_right(correction_rate):
-    list.append(correction_rate)
-    print(correction_rate)
     if correction_rate == 0:
         control(controls.yaw_right)
         control(controls.yaw_left)
+        list.append(float(get_info(yaw_error_state).rstrip('°')))
     else:
         for i in range(correction_rate):
             control(controls.yaw_right)
+            list.append(float(get_info(yaw_error_state).rstrip('°')))
         for i in range(correction_rate):
             control(controls.yaw_left)
+            list.append(float(get_info(yaw_error_state).rstrip('°')))
     print(list)
 
 
@@ -46,3 +48,11 @@ while True:
     if float(get_info(yaw_error_state).rstrip('°')) > 0.0:
         move_right(
             int(abs(kp * float(get_info(yaw_error_state).rstrip('°')))))
+    if float(get_info(yaw_error_state).rstrip('°')) == 0.0:
+        plt.plot(list, color='r')
+        plt.style.use('seaborn-bright')
+        plt.axhline(linewidth=4, color='g')
+        plt.xlabel('Time (seconds)', fontsize=18)
+        plt.ylabel('Error (degrees)', fontsize=16)
+        plt.grid()
+        plt.savefig('data/data.png')
