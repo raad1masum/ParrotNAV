9ed605bcf5402255aa224f632614cdd21130a586
raad1masum <piraadwan@gmail.com>
2020-06-05 14:51:58 -0700

    plot data

--- controller/controller.py
+++ controller/controller.py
@@ -1,7 +1,9 @@
 from controller import correct_yaw
+from data import data
 
 correct_yaw.run()
 
 while correct_yaw.get_yaw_error() != correct_yaw.target:
     correct_yaw.increment_single()
 
+data.plot()
