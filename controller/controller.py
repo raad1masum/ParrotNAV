8a2a266750dbc12e11a9c12de8ea526a2c596a06
raad1masum <piraadwan@gmail.com>
2020-06-07 15:16:38 -0700

    add status messages

--- controller/controller.py
+++ controller/controller.py
@@ -7,30 +7,35 @@ def yaw():
     correct_yaw.run()
     while correct_yaw.get_yaw_error() != correct_yaw.setpoint:
         correct_yaw.increment_single()
+    print('ParrotNAV: Yaw Correction Complete')
 
 # roll correction
 def roll():
     correct_roll.run()
     while correct_roll.get_roll_error() != correct_roll.setpoint:
         correct_roll.increment_single()
+    print('ParrotNAV: Roll Correction Complete')
 
 # pitch correction
 def pitch():
     correct_pitch.run()
     while correct_pitch.get_pitch_error() != correct_pitch.setpoint:
         correct_pitch.increment_single()
+    print('ParrotNAV: Pitch Correction Complete')
 
 # vertical correction
 def vertical():
     correct_vertical.run()
     while correct_vertical.get_vertical_error() != correct_vertical.setpoint:
         correct_vertical.increment_single()
+    print('ParrotNAV: Vertical Correction Complete')
 
 # horizontal correction
 def horizontal():
     correct_horizontal.run()
     while correct_horizontal.get_horizontal_error() != correct_horizontal.setpoint:
         correct_horizontal.increment_single()
+    print('ParrotNAV: Horizontal Correction Complete')
 
 # create threads
 yaw_thread = threading.Thread(target=yaw)
