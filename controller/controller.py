c403dff6c6cea9b0b92a9b540889af07a338344f
raad1masum <piraadwan@gmail.com>
2020-06-07 15:15:08 -0700

    get rid of is finished bools

--- controller/controller.py
+++ controller/controller.py
@@ -2,47 +2,35 @@ from controller import correct_yaw, correct_roll, correct_pitch, correct_vertica
 from data import data
 import threading
 
-# set correction finished to false
-yaw_finished = False
-roll_finished = False
-pitch_finished = False
-vertical_finished = False
-horizontal_finished = False
-
 # yaw correction
 def yaw():
     correct_yaw.run()
     while correct_yaw.get_yaw_error() != correct_yaw.setpoint:
         correct_yaw.increment_single()
-    yaw_finished = True
 
 # roll correction
 def roll():
     correct_roll.run()
     while correct_roll.get_roll_error() != correct_roll.setpoint:
         correct_roll.increment_single()
-    roll_finished = True
 
 # pitch correction
 def pitch():
     correct_pitch.run()
     while correct_pitch.get_pitch_error() != correct_pitch.setpoint:
         correct_pitch.increment_single()
-    pitch_finished = True
 
 # vertical correction
 def vertical():
     correct_vertical.run()
     while correct_vertical.get_vertical_error() != correct_vertical.setpoint:
         correct_vertical.increment_single()
-    vertical_finished = True
 
 # horizontal correction
 def horizontal():
     correct_horizontal.run()
     while correct_horizontal.get_horizontal_error() != correct_horizontal.setpoint:
         correct_horizontal.increment_single()
-    horizontal_finished = True
 
 # create threads
 yaw_thread = threading.Thread(target=yaw)
