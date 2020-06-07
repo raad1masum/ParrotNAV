eb49da8363c21ce1b31e957525021a0925aa57de
raad1masum <piraadwan@gmail.com>
2020-06-07 12:38:03 -0700

    style translation graphs

--- controller/correct_horizontal.py
+++ controller/correct_horizontal.py
@@ -58,7 +58,7 @@ def get_horizontal_error():
 
 # plot data
 def plot_data():
-    plt.plot(horizontal_data, color='y')
+    plt.plot(horizontal_data, color='b')
     plt.style.use('seaborn-bright')
     plt.axhline(linewidth=4, color='b')
     plt.xlabel('Time (seconds)', fontsize=16)
