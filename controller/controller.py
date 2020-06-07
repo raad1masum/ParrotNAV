ea3fe393b978b1e50051a7f86dcdcc6d6db8bb19
raad1masum <piraadwan@gmail.com>
2020-06-07 11:04:01 -0700

    data collection

--- controller/controller.py
+++ controller/controller.py
@@ -2,20 +2,27 @@ from controller import correct_yaw, correct_roll, correct_pitch
 from data import data
 import threading
 
+yaw_finished = False
+roll_finished = False
+pitch_finished = False
+
 def yaw():
     correct_yaw.run()
     while correct_yaw.get_yaw_error() != correct_yaw.target:
         correct_yaw.increment_single()
+    yaw_finished = True
 
 def roll():
     correct_roll.run()
     while correct_roll.get_roll_error() != correct_roll.target:
         correct_roll.increment_single()
+    roll_finished = True
 
 def pitch():
     correct_pitch.run()
     while correct_pitch.get_pitch_error() != correct_pitch.target:
         correct_pitch.increment_single()
+    pitch_finished = True
     data.plot() # this should go in the very last function
 
 yaw_run = threading.Thread(target=yaw)
