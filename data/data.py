ebb5a02b9af69bb47b1ac74be2493afc4d4c455f
raad1masum <piraadwan@gmail.com>
2020-06-07 12:09:10 -0700

    change plot to plot axis

--- data/data.py
+++ data/data.py
@@ -1,6 +1,6 @@
 from controller import correct_yaw, correct_roll, correct_pitch
 
-def plot():
+def plot_axis():
     correct_yaw.plot_data()
     correct_roll.plot_data()
     correct_pitch.plot_data()
