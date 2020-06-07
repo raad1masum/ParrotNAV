c9681f7ad16e8287ff68d2f05175481e77b44cbf
raad1masum <piraadwan@gmail.com>
2020-06-07 12:18:11 -0700

    add comments to data

--- data/data.py
+++ data/data.py
@@ -1,9 +1,11 @@
 from controller import correct_yaw, correct_roll, correct_pitch, correct_horizontal
 
+# generate axis plot
 def plot_axis():
     correct_yaw.plot_data()
     correct_roll.plot_data()
     correct_pitch.plot_data()
 
+# generate translation plot
 def plot_translation():
     correct_horizontal.plot_data()
