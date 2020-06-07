127f242bdad3991d6078611f576cf57e5f48015b
raad1masum <piraadwan@gmail.com>
2020-06-07 12:25:20 -0700

    change from target to setpoint in controller

--- controller/controller.py
+++ controller/controller.py
@@ -11,21 +11,21 @@ horizontal_finished = False
 # yaw correction
 def yaw():
     correct_yaw.run()
-    while correct_yaw.get_yaw_error() != correct_yaw.target:
+    while correct_yaw.get_yaw_error() != correct_yaw.setpoint:
         correct_yaw.increment_single()
     yaw_finished = True
 
 # roll correction
 def roll():
     correct_roll.run()
-    while correct_roll.get_roll_error() != correct_roll.target:
+    while correct_roll.get_roll_error() != correct_roll.setpoint:
         correct_roll.increment_single()
     roll_finished = True
 
 # pitch correction
 def pitch():
     correct_pitch.run()
-    while correct_pitch.get_pitch_error() != correct_pitch.target:
+    while correct_pitch.get_pitch_error() != correct_pitch.setpoint:
         correct_pitch.increment_single()
     pitch_finished = True
     data.plot_axis() # this should go in the very last axis function
@@ -33,15 +33,15 @@ def pitch():
 # horizontal correction
 def horizontal():
     correct_horizontal.run()
-    while correct_horizontal.get_horizontal_error() != correct_horizontal.target:
+    while correct_horizontal.get_horizontal_error() != correct_horizontal.setpoint:
         correct_horizontal.increment_single()
     horizontal_finished = True
     data.plot_translation() # this should go in the very last translation function
 
 # axis threads
-yaw_thread = threading.Thread(target=yaw)
-roll_thread = threading.Thread(target=roll)
-pitch_thread = threading.Thread(target=pitch)
+yaw_thread = threading.Thread(setpoint=yaw)
+roll_thread = threading.Thread(setpoint=roll)
+pitch_thread = threading.Thread(setpoint=pitch)
 
 # axis start
 yaw_thread.start()
@@ -49,7 +49,7 @@ roll_thread.start()
 pitch_thread.start()
 
 # translation threads
-horizontal_thread = threading.Thread(target=horizontal)
+horizontal_thread = threading.Thread(setpoint=horizontal)
 
 # translation start
 if yaw_finished and roll_finished and pitch_finished:
