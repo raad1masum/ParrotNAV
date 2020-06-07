7f77aec3c44c137f66457436774c40f07078fe20
raad1masum <piraadwan@gmail.com>
2020-06-07 12:12:24 -0700

    get rid of testing env

--- controller/controller.py
+++ controller/controller.py
@@ -2,29 +2,29 @@ from controller import correct_yaw, correct_roll, correct_pitch, correct_horizon
 from data import data
 import threading
 
-# yaw_finished = False
-# roll_finished = False
-# pitch_finished = False
+yaw_finished = False
+roll_finished = False
+pitch_finished = False
 horizontal_finished = False
 
-# def yaw():
-#     correct_yaw.run()
-#     while correct_yaw.get_yaw_error() != correct_yaw.target:
-#         correct_yaw.increment_single()
-#     yaw_finished = True
+def yaw():
+    correct_yaw.run()
+    while correct_yaw.get_yaw_error() != correct_yaw.target:
+        correct_yaw.increment_single()
+    yaw_finished = True
 
-# def roll():
-#     correct_roll.run()
-#     while correct_roll.get_roll_error() != correct_roll.target:
-#         correct_roll.increment_single()
-#     roll_finished = True
+def roll():
+    correct_roll.run()
+    while correct_roll.get_roll_error() != correct_roll.target:
+        correct_roll.increment_single()
+    roll_finished = True
 
-# def pitch():
-#     correct_pitch.run()
-#     while correct_pitch.get_pitch_error() != correct_pitch.target:
-#         correct_pitch.increment_single()
-#     pitch_finished = True
-#     data.plot_axis() # this should go in the very last axis function
+def pitch():
+    correct_pitch.run()
+    while correct_pitch.get_pitch_error() != correct_pitch.target:
+        correct_pitch.increment_single()
+    pitch_finished = True
+    data.plot_axis() # this should go in the very last axis function
 
 def horizontal():
     correct_horizontal.run()
@@ -33,10 +33,10 @@ def horizontal():
     horizontal_finished = True
     data.plot_translation() # this should go in the very last translation function
 
-# yaw_run = threading.Thread(target=yaw)
-# roll_run = threading.Thread(target=roll)
-# pitch_run = threading.Thread(target=pitch)
+yaw_run = threading.Thread(target=yaw)
+roll_run = threading.Thread(target=roll)
+pitch_run = threading.Thread(target=pitch)
 
-# yaw_run.start()
-# roll_run.start()
-# pitch_run.start()
+yaw_run.start()
+roll_run.start()
+pitch_run.start()
