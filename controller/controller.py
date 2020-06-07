8bc602a251d211b1224e18cd4f16ec173d0876e3
raad1masum <piraadwan@gmail.com>
2020-06-07 10:00:09 -0700

    make threads for corrections

--- controller/controller.py
+++ controller/controller.py
@@ -1,12 +1,21 @@
 from controller import correct_yaw, correct_roll
 from data import data
+import threading
 
-correct_yaw.run()
-while correct_yaw.get_yaw_error() != correct_yaw.target:
-    correct_yaw.increment_single()
+def yaw():
+    correct_yaw.run()
+    while correct_yaw.get_yaw_error() != correct_yaw.target:
+        correct_yaw.increment_single()
 
-correct_roll.run()
-while correct_roll.get_roll_error() != correct_roll.target:
-    correct_roll.increment_single()
+def roll():
+    correct_roll.run()
+    while correct_roll.get_roll_error() != correct_roll.target:
+        correct_roll.increment_single()
+
+yaw_run = threading.Thread(target=yaw)
+roll_run = threading.Thread(target=roll)
+
+yaw_run.start()
+roll_run.start()
 
 data.plot()
