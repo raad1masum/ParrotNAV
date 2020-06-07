a625d20e330d2e4c3bd4b30cf092dcd3bf4eb6fc
raad1masum <piraadwan@gmail.com>
2020-06-07 13:38:12 -0700

    plot vertical data

--- data/data.py
+++ data/data.py
@@ -1,8 +1,9 @@
-from controller import correct_yaw, correct_roll, correct_pitch, correct_horizontal
+from controller import correct_yaw, correct_roll, correct_pitch, correct_horizontal, correct_vertical
 
 # plot data
 def plot():
     correct_yaw.plot_data()
     correct_roll.plot_data()
     correct_pitch.plot_data()
+    correct_vertical.plot_data()
     correct_horizontal.plot_data()
