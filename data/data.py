c49be7f2c3a48a0275dadbad8e975e5dcbff6fbd
raad1masum <piraadwan@gmail.com>
2020-06-07 12:09:55 -0700

    create plot translation

--- data/data.py
+++ data/data.py
@@ -1,6 +1,9 @@
-from controller import correct_yaw, correct_roll, correct_pitch
+from controller import correct_yaw, correct_roll, correct_pitch, correct_horizontal
 
 def plot_axis():
     correct_yaw.plot_data()
     correct_roll.plot_data()
     correct_pitch.plot_data()
+
+def plot_translation():
+    correct_horizontal.plot_data()
