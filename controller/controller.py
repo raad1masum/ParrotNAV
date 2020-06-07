4ee98c93f013bdf7bddc8a1332afd106b4a664dd
raad1masum <piraadwan@gmail.com>
2020-06-07 09:40:49 -0700

    plot roll data

--- controller/controller.py
+++ controller/controller.py
@@ -1,14 +1,12 @@
 from controller import correct_yaw, correct_roll
 from data import data
 
+correct_yaw.run()
+while correct_yaw.get_yaw_error() != correct_yaw.target:
+    correct_yaw.increment_single()
+
 correct_roll.run()
 while correct_roll.get_roll_error() != correct_roll.target:
     correct_roll.increment_single()
 
-correct_roll.plot_data()
-
-# correct_yaw.run()
-# while correct_yaw.get_yaw_error() != correct_yaw.target:
-#     correct_yaw.increment_single()
-
-# data.plot()
+data.plot()
--- data/data.py
+++ data/data.py
@@ -1,4 +1,5 @@
-from controller import correct_yaw
+from controller import correct_yaw, correct_roll
 
 def plot():
     correct_yaw.plot_data()
+    correct_roll.plot_data()
