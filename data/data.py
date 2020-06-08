9ab8ebfad8407c95a687d40dedbef0d18ae5e29a
raad1masum <piraadwan@gmail.com>
2020-06-08 11:44:51 -0700

    plot range data

--- data/data.py
+++ data/data.py
@@ -1,9 +1,10 @@
-from controller import correct_yaw, correct_roll, correct_pitch, correct_vertical, correct_horizontal
+from controller import correct_yaw, correct_roll, correct_pitch, correct_range, correct_vertical, correct_horizontal
 
 # plot data
 def plot():
     correct_yaw.plot_data()
     correct_roll.plot_data()
     correct_pitch.plot_data()
+    correct_range.plot_data()
     correct_vertical.plot_data()
     correct_horizontal.plot_data()
