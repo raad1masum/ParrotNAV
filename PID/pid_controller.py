0c095fbe6c2ed2fc0259a8ee657bf9565c26aba8
raad1masum <piraadwan@gmail.com>
2020-06-08 09:44:49 -0700

    tune PID

--- PID/pid_controller.py
+++ PID/pid_controller.py
@@ -1,10 +1,10 @@
 yaw_kp = 0.47
 yaw_setpoint = 0.0
-roll_kp = 0.39
+roll_kp = 0.40
 roll_setpoint = 0.0
-pitch_kp = 0.35
+pitch_kp = 0.38
 pitch_setpoint = 0.0
-horizontal_kp = 0.43
+horizontal_kp = 0.42
 horizontal_setpoint = 0.0
 vertical_kp = 0.25
 vertical_setpoint = 0.0
